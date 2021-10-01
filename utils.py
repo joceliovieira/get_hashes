cc0947b3b8deaff9c1796ef3cbd5804140b63703
Jocelio Vieira <40965018+joceliovieira@users.noreply.github.com>
2021-09-30 21:30:55 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,14 +23,8 @@ def get_hashes(sigla_input):
     var_error = []
     var_ok = []
     cont = 0
-
-#     print("Digite o tipo de Proposição a ser analisada.\n\
-# Tipos suportados: PEC, PLP, PL.\n\
-# Os tipos devem ser escritos serparados por vírgulas e sem espaços vazios entre as siglas.")
-
-    siglaTipo = str(sigla_input) # Pode ser substituido por um valor requisitado via API
+    siglaTipo = str(sigla_input)
     siglas = [item.strip() for item in siglaTipo.split(sep=',')]
-
     try:
       for i in siglas:
         cont+=1
@@ -51,7 +45,6 @@ def get_hashes(sigla_input):
           'data': "O tipo da seguinte proposicao nao e adequado: " + str(var_error[0]),
           'status_code': 400
         }
-        # print("Insira o tipo de proposição novamente.")
       else:
         print(f"Os tipos das seguintes proposições não são adequados: %s " % str(var_error))
         print("Insira os tipos novamente.")
@@ -59,23 +52,15 @@ def get_hashes(sigla_input):
     
     print(f"Coletando informações sobre as proposições do tipo %s apresentadas nos últimos 3 dias." % var_ok)
     response = requests.get(url, params)
-    # if response.json()['data'] == []:
-    # if (pd.json_normalize(response.json())['data'] == []):
-      # print("Não há Proposições apresentadas no período de análise")
-      # return({"status_code":400})
-    # else:
     id_serie = pd.json_normalize(response.json()['dados'])[['id','siglaTipo']]
-    # requests_list = [url + str(i) for i in id_serie]
     requests_list = [url + str(i) for i in id_serie['id']]
     dados_iniciais = [(j['id'], j['siglaTipo'], j['urlInteiroTeor']) for j in [requests.get(i).json()['dados'] for i in requests_list]]
     df_01 = pd.DataFrame(dados_iniciais, columns=['id', 'siglaTipo', 'urlInteiroTeor'])
-    # lista_hashes = [(df_01.iloc[i]['id'], df_01.iloc[i]['siglaTipo'], get_hash(df_01.iloc[i]['urlInteiroTeor'])) for i in range(0,len(df_01))]
     lista_hashes = [(df_01.iloc[i]['id'], get_hash(df_01.iloc[i]['urlInteiroTeor'])) for i in range(0,len(df_01))]
     df_02 = pd.DataFrame(lista_hashes, columns=['id', 'hashmd5'])
     df_final = df_02.merge(df_01, on='id')
     ld_final = json.loads(df_final.drop(columns=['urlInteiroTeor']).to_json(orient='records'))
     lista_final_hashes = [i['hashmd5'] for i in ld_final]    
-    # print(ld_final)
     return {
       'data': ld_final,
       'status_code': 200
